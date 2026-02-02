c67f9a09317ac675cba20d0e55a66b434a45f2d7
BirhanuB123 <birhanubayne@gmail.com>
2026-02-02 10:18:41 +0800

    new updates on json files

--- frontend/src/apps/Header/HeaderContainer.jsx
+++ frontend/src/apps/Header/HeaderContainer.jsx
@@ -129,5 +129,5 @@ export default function HeaderContent() {
 }
 
 //  console.log(
-//    '🚀 Welcome to INTEGRA ERP CRM! Did you know that we also offer commercial customization services? Contact us at hello@idurarapp.com for more information.'
+//    '🚀 Welcome to INTEGRA ERP CRM! Did you know that we also offer commercial customization services? Contact us at info@gebetatech.com for more information.'
 //  );
